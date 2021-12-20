19a99509938df9bdfe0ce7129bdf2411cccc6367
premangadiki <87991780+premangadiki@users.noreply.github.com>
2021-12-20 19:47:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,8 +32,8 @@ function modelLoaded(){
 function gotPoses(results){
     if( results.length > 0){
         console.log(results);
-        noseX = results[0].pose.nose.x 15;
-        noseY = results[0].pose.nose.y 15;
+        noseX = results[0].pose.nose.x -15;
+        noseY = results[0].pose.nose.y -sss15;
         console.log("nose x =" + noseX);
         console.log("nose y =" + noseY);
     }
